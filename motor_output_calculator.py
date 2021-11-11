dc49c06b123a3b845e7905c585b012aaaf45cdcc
michaellee8 <ckmichael8@gmail.com>
2021-11-11 11:31:45 +0000

    feat: wip

--- motor_output_calculator.py
+++ motor_output_calculator.py
@@ -74,7 +74,7 @@ class MotorOutputCalculator(object):
                 # car at camera center, very good!
                 pass
 
-            mov_vec = times_list(BACKWARD_VEC, FORWARD_COEFFICIENT * 0.5)
+            mov_vec = times_list(BACKWARD_VEC, FORWARD_COEFFICIENT * 0.6)
             # estimated_angle_diff = (
                 # self.cur_pan_angle - 90 + 30 * cam_x_diff / (CAM_WIDTH/2)) % 360.0
             estimated_angle_diff = (
@@ -86,8 +86,8 @@ class MotorOutputCalculator(object):
 
         
 
-            if self.left_sonar_distance < DESIRED_DISTANCE or self.right_sonar_distance < DESIRED_DISTANCE:
+            if self.left_sonar_distance < DESIRED_DISTANCE*3/4 or self.right_sonar_distance < DESIRED_DISTANCE*3/4:
                 print(self.left_sonar_distance, self.right_sonar_distance)
-                mov_vec = times_list(mov_vec, 0.25)
+                mov_vec = times_list(mov_vec, 0.05)
 
             (_, angle_diff, guessed_car_side, cam_x_diff, timestamp) = yield (mov_vec[0], mov_vec[1], mov_vec[2], mov_vec[3], next_pan_angle, next_tilt_angle, timestamp)
